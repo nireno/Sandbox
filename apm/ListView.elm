3fe56a8e7a2504c997e134016f814c7d210e1d5a
Ixanos <info@ixanos.co.tt>
2016-07-16 20:09:48 -0400

    wip: add master selection dropdown

--- apm/ListView.elm
+++ apm/ListView.elm
@@ -5,7 +5,7 @@ import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http exposing (RawError, Response)
-import Json.Decode exposing (..)
+import Json.Decode
 import Json.Encode
 import Task exposing (Task)
 import HttpBuilder
@@ -17,7 +17,7 @@ import Maybe exposing (withDefault)
 
 main =
   Html.program
-    { init = init rpc_project
+    { init = init rpc_task
     , view = view
     , update = update
     , subscriptions = subscriptions
@@ -32,21 +32,24 @@ rpc_project =
     }
 
 rpc_task = 
-    { url = {get = "http://localhost:3000/v_project_task?select=id,project,task", set = ""}
-    , fields = ["id", "project", "task"]
-    , prettyFields = ["ID", "Project", "Task"]
+    { url = {get = "http://localhost:3000/project_task?select=id,project_id,name,estimated_hours", set = "http://localhost:3000/project_task"}
+    , fields = 
+        [ {name = "id", value = "", prettyName = "ID", encoder = Json.Encode.string, editable=False, hasMaster=False}
+        , {name="project_id", value="", prettyName="Project", encoder = \s -> String.toInt s |> Result.withDefault 0 |> Json.Encode.int, editable=True, hasMaster=True}
+        , {name="name", value="", prettyName="Task", encoder = Json.Encode.string, editable=True, hasMaster=False}
+        , {name="estimated_hours", value="", prettyName="Estimate", encoder = \s -> String.toInt s |> Result.withDefault 0 |> Json.Encode.int, editable=True, hasMaster=False}
+        ]
+    , masters = [ {field = "project_id", url = "http://localhost:3000/project?select=id,name", items = []} ]
     }
 
 init rpc = 
-    (
         { things = []
         , newThingName = ""
         , selectedThingId = 0
         , prevSelectedThingId = 0
         , rpc = rpc
         }
-    , cmdGetThings rpc.fields rpc.url.get
-    )
+        ! (cmdGetThings rpc.fields rpc.url.get :: List.map (\m -> cmdGetMaster m) rpc.masters)
 
 type Msg
   = FetchSucceed (List Thing)
@@ -55,6 +58,7 @@ type Msg
   | PostSucceed Http.Response
   | PostFail Http.RawError
   | ButtonClick
+  | ReadMasterSucceed String (List (Int, String))
   --| KeyUp Int
   --| SelectThing Int
   --| DeleteSucceed Http.Response
@@ -69,6 +73,7 @@ type alias Model =
     , rpc: 
         { url: { get: String, set: String}
         , fields: List Field
+        , masters: List MasterField
         }
     }
 
@@ -78,7 +83,16 @@ type alias Field =
     , prettyName: String
     , encoder: String -> Json.Encode.Value
     , editable: Bool
+    , hasMaster: Bool
     }
+
+type alias MasterField = 
+    { field: String
+    , url: String
+    , items: List (Int, String)
+    }
+
+masterFieldTemplate = {field = "", url = "", items = []}
 
 type alias Thing = List String
 
@@ -91,10 +105,17 @@ update action model =
         let 
             fields = List.map (\f -> if f.name == fieldName then {f | value = str} else f) model.rpc.fields
         in
-            ({ model | rpc = {url = model.rpc.url, fields = fields} }, Cmd.none)
+            ({ model | rpc = {url = model.rpc.url, fields = fields, masters = model.rpc.masters} }, Cmd.none)
     PostSucceed _ -> (model, cmdGetThings model.rpc.fields model.rpc.url.get )
     PostFail _-> (model, Cmd.none)
     ButtonClick -> (model, cmdPostThing model.rpc.url.set model.rpc.fields)
+    ReadMasterSucceed fieldname pairs -> 
+        let 
+            masters = List.map (\m -> {m | items = if m.field == fieldname then pairs else m.items}) model.rpc.masters
+            rpc = model.rpc
+            newRpc = {rpc | masters = masters}
+        in 
+            ({model | rpc = newRpc}, Cmd.none)
     --DeleteSucceed _ -> ({ model | selectedThingId = nextThingId model.things model.selectedThingId}, cmdGetThings model.thingUrl)
     --KeyUp keycode ->
     --        if keycode == enterKeycode && String.length model.newThingName > 0 then
@@ -122,7 +143,19 @@ view model =
             ]
         ]
 
+        masters = model.rpc.masters
         prettyFields = List.map (\f -> f.prettyName) model.rpc.fields
+        getMaster masters fieldName = List.filter (\m -> m.field == fieldName) masters |> List.head |> Maybe.withDefault masterFieldTemplate
+        masterDropdown fieldName =
+            let 
+                master = getMaster masters fieldName
+                msgOptionChanged val = Change fieldName val
+                options = 
+                    List.map 
+                        (\(id, name) -> option [value (toString id)] [text name]) 
+                        master.items
+            in 
+                select [onChange msgOptionChanged] options
     in
         div []
             [ table []
@@ -132,9 +165,11 @@ view model =
                     (\thing -> 
                         tr [] (List.map (\field -> td [] [text field]) thing) )
                     model.things)
-                    [tr [] (List.map (\f -> td [] [input [onInput (Change f.name), disabled (not f.editable)] []]) model.rpc.fields)]
+                    [tr [] (List.map (\f -> td [] [if f.hasMaster then masterDropdown f.name else input [onInput (Change f.name), disabled (not f.editable)] []] ) model.rpc.fields)]
                 )
             , button [onClick ButtonClick] [text "new thing"]
+            , div [] (List.map (\m -> p [] [text (toString m.items)]) masters)
+            , div [] [text ( toString model.things) ]
             --, p [] [ jsonifyThing ["name"] ["niren"] |> toString |> text ]
             --, input (inputAttribs model) []
             --, ul [] (List.map (\thing -> li (listItemAttribs thing.id) [text thing.name]) model.things )
@@ -162,21 +197,34 @@ inputAttribs model =
 --cmdGetThings =
 --    Task.perform FetchFail FetchSucceed (Http.get thingsDecoder (url ++ "?select=id,name"))
 
+decodeToString = 
+    let 
+        stringify a = toString a
+    in 
+        Json.Decode.oneOf 
+        [ Json.Decode.string
+        , Json.Decode.map stringify Json.Decode.int
+        , Json.Decode.null "null"
+        ]
+
 cmdGetThings : List Field -> String -> Cmd Msg
 cmdGetThings fields url =
     let
         fieldNames = List.map (\f -> f.name) fields
-        stringify a = toString a
-        something = 
-            Json.Decode.oneOf 
-            [ Json.Decode.string
-            , Json.Decode.map stringify Json.Decode.int
-            ]
 
-        dictToList = Json.Decode.map (\d -> List.map (\f -> withDefault "?" (Dict.get f d)) fieldNames) (Json.Decode.dict something)
+        dictToList = Json.Decode.map (\d -> List.map (\f -> withDefault "?" (Dict.get f d)) fieldNames) (Json.Decode.dict decodeToString)
         decoder = Json.Decode.list (dictToList)
     in
         Task.perform XhrFail FetchSucceed (Http.get decoder url)
+
+cmdGetMaster : MasterField -> Cmd Msg
+cmdGetMaster master =
+    let
+        dictToPair = Json.Decode.map (\d -> (Dict.get "id" d |> withDefault "0" |> String.toInt |> Result.withDefault 0, Dict.get "name" d |> withDefault "<error>") ) (Json.Decode.dict decodeToString)
+        decoder = Json.Decode.list (dictToPair)
+    in
+        Task.perform XhrFail (ReadMasterSucceed master.field) (Http.get decoder master.url)
+
 
 jsonifyThing: List Field -> Http.Body
 jsonifyThing fields =
@@ -231,3 +279,7 @@ nextThingId things thingId =
         case fun things thingId of
             [] -> 0
             hd::tl -> hd.id
+
+onChange : (String -> msg) -> Attribute msg
+onChange tagger =
+  on "change" (Json.Decode.map tagger targetValue)

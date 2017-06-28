b4678cd3492b5f37ac6ab9d41c45b4350b623076
Niren Orie <nireno@gmail.com>
2017-06-28 01:58:32 -0400

    remove unnecessary code

--- hello_sicp/ex1.6.rkt
+++ hello_sicp/ex1.6.rkt
@@ -1,35 +1,5 @@
 #lang sicp
 
-(define (average x y) (/ (+ x y) 2))
-
-(define (square x) (* x x) )
-
-(define (good-enough? guess x)
-  (if (< (abs (- x (square guess))) 0.001)
-      true
-      false))
-
-(define (improve guess x)
-  (average guess (/ x guess)))
-
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
-      guess
-      (sqrt-iter (improve guess x) x)))
-
-(sqrt-iter 1.0 25)
-
-(define (new-if predicate consequent alternative)
-  (cond (predicate consequent)
-        (else alternative)))
-
-(new-if (good-enough? 2 3) true false)
-
-(define (new-sqrt-iter guess x)
-  (new-if (good-enough? guess x)
-      guess
-      (new-sqrt-iter (improve guess x) x)))
-
 ;;;; Exercise 1.6
 ;;;; Why does if need to be provided as a special form?
 
